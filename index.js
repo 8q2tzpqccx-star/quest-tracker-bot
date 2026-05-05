d6625edaed03211bc9e5f967e2eec5822cf2a870
Linnea Lagumbay <linnealagumbay@Linneas-Air.ht.home>
2026-05-05 20:42:32 -0300

    add login success log

--- index.js
+++ index.js
@@ -210,6 +210,6 @@ console.log('TOKEN exists:', !!process.env.TOKEN);
 console.log('CLIENT_ID exists:', !!process.env.CLIENT_ID);
 console.log('GUILD_ID exists:', !!process.env.GUILD_ID);
 
-client.login(process.env.TOKEN).catch(err => {
-  console.log('Login failed:', err.message);
-});
+client.login(process.env.TOKEN)
+  .then(() => console.log('LOGIN SUCCESS'))
+  .catch(err => console.log('LOGIN FAILED:', err.message));
